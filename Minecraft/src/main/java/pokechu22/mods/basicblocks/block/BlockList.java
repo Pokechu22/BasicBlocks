404da40a399ec78a6a2945524aac128c353ea1ce
Pokechu22 <Pokechu022@gmail.com>
2014-09-24 15:23:04 -0700

    Fixed light level.

--- Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockList.java
+++ Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockList.java
@@ -26,37 +26,37 @@ public class BlockList {
 	public final static Block baseBlock = new BasicBlock(Material.iron).setHardness(1)
 			.setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("baseBlock").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block slopeNorth = new BlockSlope(Material.iron, SlopeType.NORTH)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("slopeNorth").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block slopeEast = new BlockSlope(Material.iron, SlopeType.EAST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("slopeEast").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block slopeSouth = new BlockSlope(Material.iron, SlopeType.SOUTH)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("slopeSouth").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block slopeWest = new BlockSlope(Material.iron, SlopeType.WEST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("slopeWest").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block cornerNorthEast = new BlockCorner(Material.iron, CornerType.NORTH_EAST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("cornerNorthEast").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block cornerNorthWest = new BlockCorner(Material.iron, CornerType.NORTH_WEST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("cornerNorthWest").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block cornerSouthEast = new BlockCorner(Material.iron, CornerType.SOUTH_EAST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("cornerSouthEast").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 	public final static Block cornerSouthWest = new BlockCorner(Material.iron, CornerType.SOUTH_WEST)
 			.setHardness(1).setStepSound(Block.soundTypeMetal).setCreativeTab(tabBasicBlocks)
 			.setBlockName("cornerSouthWest").setBlockTextureName("basicblocks:BaseTexture")
-			.setLightLevel(15);
+			.setLightLevel(1);
 }
