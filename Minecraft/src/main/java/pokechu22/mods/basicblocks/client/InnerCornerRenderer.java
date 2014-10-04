fde0c9230eb16f419db70f71ce494413ec3037df
Pokechu22 <Pokechu022@gmail.com>
2014-10-03 21:15:07 -0700

    Fixed side rendering now.
    For future reference: The .5 section needs to be either the 2nd item or
    the 4th.

--- Minecraft/src/main/java/pokechu22/mods/basicblocks/client/InnerCornerRenderer.java
+++ Minecraft/src/main/java/pokechu22/mods/basicblocks/client/InnerCornerRenderer.java
@@ -144,15 +144,15 @@ public class InnerCornerRenderer implements ISimpleBlockRenderingHandler {
 		t.addVertexWithUV(x + 1, y + 1, z + 0, maxU, minV);
 		t.addVertexWithUV(x + .5, y + 1, z + .5, minU, minV);
 		//South
+		t.addVertexWithUV(x + 0, y + 0, z + 0, minU, minV);
 		t.addVertexWithUV(x + 0, y + 0, z + 1, maxU, minV);
 		t.addVertexWithUV(x + 0, y + 1, z + 1, maxU, maxV);
 		t.addVertexWithUV(x + 0, y + .5, z + .5, minU, maxV);
-		t.addVertexWithUV(x + 0, y + 0, z + 0, minU, minV);
 		//East
+		t.addVertexWithUV(x + 1, y + 1, z + 0, minU, minV);
 		t.addVertexWithUV(x + 1, y + 0, z + 0, maxU, minV);
 		t.addVertexWithUV(x + 0, y + 0, z + 0, maxU, maxV);
 		t.addVertexWithUV(x + .5, y + .5, z + 0, minU, maxV);
-		t.addVertexWithUV(x + 1, y + 1, z + 0, minU, minV);
 		//North
 		t.addVertexWithUV(x + 1, y + 0, z + 0, minU, minV);
 		t.addVertexWithUV(x + 1, y + 1, z + 0, maxU, minV);
@@ -186,15 +186,15 @@ public class InnerCornerRenderer implements ISimpleBlockRenderingHandler {
 		t.addVertexWithUV(x + 1, y + 1, z + 0, maxU, minV);
 		t.addVertexWithUV(x + 0, y + 1, z + 0, minU, minV);
 		//North
+		t.addVertexWithUV(x + 1, y + 0, z + 1, maxU, minV);
 		t.addVertexWithUV(x + 1, y + 0, z + 0, minU, minV);
 		t.addVertexWithUV(x + 1, y + 1, z + 0, minU, maxV);
 		t.addVertexWithUV(x + 1, y + .5, z + .5, maxU, maxV);
-		t.addVertexWithUV(x + 1, y + 0, z + 1, maxU, minV);
 		//East
+		t.addVertexWithUV(x + 0, y + 1, z + 1, maxU, minV);
 		t.addVertexWithUV(x + 0, y + 0, z + 1, minU, minV);
 		t.addVertexWithUV(x + 1, y + 0, z + 1, minU, maxV);
 		t.addVertexWithUV(x + .5, y + .5, z + 1, maxU, maxV);
-		t.addVertexWithUV(x + 0, y + 1, z + 1, maxU, minV);
 		//South
 		t.addVertexWithUV(x + 0, y + 0, z + 1, minU, maxV);
 		t.addVertexWithUV(x + 0, y + 1, z + 1, maxU, maxV);
