cbc984ff570dfe6065e44721465548a33aafc2cf
Pokechu22 <Pokechu022@gmail.com>
2014-10-11 22:53:17 -0700

    Hitboxes for shallow slopes.

--- Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockShallowSlope.java
+++ Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockShallowSlope.java
@@ -11,14 +11,26 @@ import net.minecraft.util.IIcon;
 public class BlockShallowSlope extends Block {
 
 	public static enum SlopeType {
-		UPPER_NORTH,
-		UPPER_EAST,
-		UPPER_SOUTH,
-		UPPER_WEST,
-		LOWER_NORTH,
-		LOWER_EAST,
-		LOWER_SOUTH,
-		LOWER_WEST
+		UPPER_NORTH(0,0,0,1,1,1),
+		UPPER_EAST(0,0,0,1,1,1),
+		UPPER_SOUTH(0,0,0,1,1,1),
+		UPPER_WEST(0,0,0,1,1,1),
+		LOWER_NORTH(0,0,0,1,.5,1),
+		LOWER_EAST(0,0,0,1,.5,1),
+		LOWER_SOUTH(0,0,0,1,.5,1),
+		LOWER_WEST(0,0,0,1,.5,1);
+
+		private SlopeType(double minX, double minY, double minZ, double maxX,
+				double maxY, double maxZ) {
+			this.minX = minX;
+			this.minY = minY;
+			this.minZ = minZ;
+			this.maxX = maxX;
+			this.maxY = maxY;
+			this.maxZ = maxZ;
+		}
+
+		public final double minX, minY, minZ, maxX, maxY, maxZ;
 	}
 	
 	public final SlopeType type;
@@ -26,6 +38,13 @@ public class BlockShallowSlope extends Block {
 	public BlockShallowSlope(Material material, SlopeType type) {
 		super(material);
 		this.type = type;
+		
+		this.minX = type.minX;
+		this.minY = type.minY;
+		this.minZ = type.minZ;
+		this.maxX = type.maxX;
+		this.maxY = type.maxY;
+		this.maxZ = type.maxZ;
 	}
 
 	@Override
