fe23972b1d899ec9a47390c848fbd606ece1cf5f
Pokechu22 <Pokechu022@gmail.com>
2014-10-11 23:23:14 -0700

    Collision boxes for corners.

--- Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockCorner.java
+++ Minecraft/src/main/java/pokechu22/mods/basicblocks/block/BlockCorner.java
@@ -9,10 +9,22 @@ import net.minecraft.block.material.Material;
 public class BlockCorner extends Block {
 
 	public static enum CornerType {
-		NORTH_EAST,
-		NORTH_WEST,
-		SOUTH_EAST,
-		SOUTH_WEST
+		NORTH_EAST(.5,0,0,  1,.5,.5),
+		NORTH_WEST(0,0,0,   .5,.5,.5),
+		SOUTH_EAST(.5,0,.5, 1,.5,1),
+		SOUTH_WEST(0,0,.5,  .5,.5,1);
+		
+		private CornerType(double minX, double minY, double minZ, double maxX,
+				double maxY, double maxZ) {
+			this.minX = minX;
+			this.minY = minY;
+			this.minZ = minZ;
+			this.maxX = maxX;
+			this.maxY = maxY;
+			this.maxZ = maxZ;
+		}
+
+		public final double minX, minY, minZ, maxX, maxY, maxZ;
 	}
 	
 	public final CornerType type;
@@ -20,7 +32,13 @@ public class BlockCorner extends Block {
 	public BlockCorner(Material material, CornerType type) {
 		super(material);
 		this.type = type;
-		// TODO Auto-generated constructor stub
+		
+		this.minX = type.minX;
+		this.minY = type.minY;
+		this.minZ = type.minZ;
+		this.maxX = type.maxX;
+		this.maxY = type.maxY;
+		this.maxZ = type.maxZ;
 	}
 
 	@Override
